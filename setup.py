fbe7763c487dd2d336b55dae6a6b45fc56a009fa
ogpgit <osvaldogpires@gmail.com>
2020-10-19 06:49:07 -0300

    Implementando a integração contínua de teste no .trevis.yml
      close #20

--- setup.py
+++ setup.py
@@ -19,8 +19,10 @@ standard_exclude_directories = [
 ]
 
 
-# (c) 2005 Ian Bicking and contributors; written for Paste (http://pythonpaste.org)
-# Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+# (c) 2005 Ian Bicking and contributors; written for Paste
+# (http://pythonpaste.org)
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license.php
 # Note: you may want to copy this into your setup.py file verbatim, as
 # you can't import this from another package, when you don't know if
 # that package is installed yet.
@@ -76,7 +78,8 @@ def find_package_data(
                         new_package = package + "." + name
                     stack.append((fn, "", new_package, False))
                 else:
-                    stack.append((fn, prefix + name + "/", package, only_in_packages))
+                    stack.append((fn, prefix + name + "/",
+                                  package, only_in_packages))
             elif package or not only_in_packages:
                 # is a file
                 bad_name = False
@@ -96,7 +99,8 @@ def find_package_data(
 
 PACKAGE = "libpythonpro"
 NAME = PACKAGE
-DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
+DESCRIPTION = "Módulo para exemplificar construção de projetos " \
+              "Python no curso PyTools"
 AUTHOR = "Osvaldo Pires"
 AUTHOR_EMAIL = "osvaldogpires@gmail.com"
 URL = "https://github.com/ogpgit/libpythonpro"
@@ -118,7 +122,8 @@ setup(
         "Development Status :: 2 - Pre-Alpha",
         "Environment :: Console",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "License :: OSI Approved :: GNU Affero General Public License "
+        "v3 or later (AGPLv3+)",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.6",
