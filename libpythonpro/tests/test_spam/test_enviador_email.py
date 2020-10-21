6ad4156d00db2c9b41314cf09e6c8bc7253d3136
ogpgit <osvaldogpires@gmail.com>
2020-10-21 07:17:08 -0300

    Implementando o projeto usando a técnica de TDD e Baby Step
    close #21

--- libpythonpro/tests/test_spam/test_enviador_email.py
+++ libpythonpro/tests/test_spam/test_enviador_email.py
@@ -1,1 +1,17 @@
-# teste
+from libpythonpro.spam.enviador_email import Enviador
+
+
+def test_criar_enviador_de_email():
+    enviador = Enviador()
+    assert enviador is not None
+
+
+def test_remetente():
+    enviador = Enviador()
+    resultado = enviador.enviar(
+        'osvaldogpires@gmail.com',
+        'osvaldo-gp@detran.go.gov.br',
+        'Cursos Python Pro',
+        'Primeira turma Guido Von Rossum aberta.'
+    )
+    assert 'osvaldogpires@gmail.com' in resultado
