e9c99c113e3a93ebe3c5855d4b6d1459d0e5aab6
ogpgit <osvaldogpires@gmail.com>
2020-10-30 08:31:55 -0300

    Implementando Implementação de Conexão, Sessão e Usuário

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -5,7 +5,7 @@ from libpythonpro.spam.modelos import Usuario
 def test_salvar_usuario():
     conexao = Conexao()
     sessao = conexao.gerar_sessão()
-    usuario = Usuario(nome ='Osvaldo')
+    usuario = Usuario(nome='Osvaldo')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
     sessao.roll_back()
@@ -16,7 +16,7 @@ def test_salvar_usuario():
 def test_listar_usuarios():
     conexao = Conexao()
     sessao = conexao.gerar_sessão()
-    usuarios = [Usuario(nome ='Osvaldo'), Usuario(nome ='Silvia')]
+    usuarios = [Usuario(nome='Osvaldo'), Usuario(nome='Silvia')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar_usuarios()
