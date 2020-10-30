31dca404862f6774d156f9eb4b01f123996faf38
ogpgit <osvaldogpires@gmail.com>
2020-10-30 08:11:59 -0300

    Implementando Implementação de Conexão, Sessão e Usuário

---
+++ libpythonpro/spam/db.py
@@ -0,0 +1,24 @@
+class Sessao:
+    contador = 0
+    usuarios = []
+    def salvar(self, usuario):
+        Sessao.contador += 1
+        usuario.id = Sessao.contador
+        self.usuarios.append(usuario)
+
+    def listar_usuarios(self):
+        return self.usuarios
+
+    def roll_back(self):
+        pass
+
+    def fechar(self):
+        pass
+
+
+class Conexao:
+    def gerar_sessão(self):
+        return Sessao()
+
+    def fechar(self):
+        pass
--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -1,7 +1,11 @@
+from libpythonpro.spam.db import Conexao
+from libpythonpro.spam.modelos import Usuario
+
+
 def test_salvar_usuario():
     conexao = Conexao()
     sessao = conexao.gerar_sessão()
-    usuario = Usuario(nome = 'Osvaldo')
+    usuario = Usuario(nome ='Osvaldo')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
     sessao.roll_back()
@@ -9,13 +13,13 @@ def test_salvar_usuario():
     conexao.fechar()
 
 
-def test_liatar_usuarios():
+def test_listar_usuarios():
     conexao = Conexao()
     sessao = conexao.gerar_sessão()
-    usuarios = [Usuario(nome = 'Osvaldo'), Usuario(nome = 'Silvia')]
+    usuarios = [Usuario(nome ='Osvaldo'), Usuario(nome ='Silvia')]
     for usuario in usuarios:
         sessao.salvar(usuario)
-    assert usuarios == sessao.listar()
+    assert usuarios == sessao.listar_usuarios()
     sessao.roll_back()
     sessao.fechar()
     conexao.fechar()
