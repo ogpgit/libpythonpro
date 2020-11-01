63a5f8024056bccabda5d995056a0bd921516d3e
ogpgit <osvaldogpires@gmail.com>
2020-11-01 09:35:09 -0300

    Implementando Setup e Tear Down com Fixture

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -1,25 +1,36 @@
+import pytest
+
 from libpythonpro.spam.db import Conexao
 from libpythonpro.spam.modelos import Usuario
 
 
-def test_salvar_usuario():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessão()
+@pytest.fixture
+def conexao():
+    # Setup
+    conexao_obj = Conexao()
+    yield conexao_obj
+    # Tear Down
+    conexao_obj.fechar()
+
+
+@pytest.fixture
+def sessao(conexao):
+    # Setup
+    sessao_obj = conexao.gerar_sessão()
+    yield sessao_obj
+    # Tear Down
+    sessao_obj.roll_back()
+    sessao_obj.fechar()
+
+
+def test_salvar_usuario(sessao):
     usuario = Usuario(nome='Osvaldo')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
 
 
-def test_listar_usuarios():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessão()
+def test_listar_usuarios(sessao):
     usuarios = [Usuario(nome='Osvaldo'), Usuario(nome='Silvia')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar_usuarios()
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
