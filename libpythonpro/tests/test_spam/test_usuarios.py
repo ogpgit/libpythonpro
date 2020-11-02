82a83e4308f6a3fedf96ff896277e0e1017f91fc
ogpgit <osvaldogpires@gmail.com>
2020-11-02 08:52:29 -0300

    Implementando Arquivo Conftest Nesse tópico você vai aprender como permitir que fixtures sejam reutilizadas por diferentes módulos utilizando o arquivo conftest.py

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -1,26 +1,4 @@
-import pytest
-
-from libpythonpro.spam.db import Conexao
 from libpythonpro.spam.modelos import Usuario
-
-
-@pytest.fixture(scope='session')
-def conexao():
-    # Setup
-    conexao_obj = Conexao()
-    yield conexao_obj
-    # Tear Down
-    conexao_obj.fechar()
-
-
-@pytest.fixture
-def sessao(conexao):
-    # Setup
-    sessao_obj = conexao.gerar_sessão()
-    yield sessao_obj
-    # Tear Down
-    sessao_obj.roll_back()
-    sessao_obj.fechar()
 
 
 def test_salvar_usuario(sessao):
