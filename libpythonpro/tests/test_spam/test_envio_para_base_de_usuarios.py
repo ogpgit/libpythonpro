69296c178dbbfdba2dad42efd7c0fbf48e933654
ogpgit <osvaldogpires@gmail.com>
2020-11-05 06:37:38 -0300

    Implementando Produção de Código Testável - Nesse capítulo você vai notar que nem todo código e testável e que as vezes é necessário fazer alterações afim de produzir testes.

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,11 +1,30 @@
+import pytest
+
 from libpythonpro.spam.enviador_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
+from libpythonpro.spam.modelos import Usuario
 
 
-def test_envio_de_spam(sessao):
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Osvaldo', email='osvaldogpires@gmail.com'),
+            Usuario(nome='Silvia', email='silvia1957@gmail.com')
+        ],
+        [
+            Usuario(nome='Osvaldo', email='osvaldogpires@gmail.com')
+        ]
+    ]
+)
+def test_qtde_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = Enviador()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'osvaldogpires@gmail.com',
         'Curso Python Pro',
         'Confira os módulos fantasticos'
     )
+    assert len(usuarios) == enviador.qtd_email_enviados
--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -2,13 +2,16 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Osvaldo')
+    usuario = Usuario(nome='Osvaldo', email='osvaldogpires@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(sessao):
-    usuarios = [Usuario(nome='Osvaldo'), Usuario(nome='Silvia')]
+    usuarios = [
+        Usuario(nome='Osvaldo', email='osvaldogpires@gmail.com'),
+        Usuario(nome='Silvia', email='silvia1957@gmail.com')
+    ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar_usuarios()
