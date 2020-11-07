1e0b73dd0f24d0a199498e2f982956a752b24007
ogpgit <osvaldogpires@gmail.com>
2020-11-07 07:56:47 -0300

    Implementando - Injeção de Dependências - Nesse tópico você vai aprender o conceito de injeção de dependências para poder produzir teste unitários

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -20,7 +20,7 @@ from libpythonpro.spam.modelos import Usuario
 def test_qtde_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador()
+    enviador = EnviadorMock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'osvaldogpires@gmail.com',
@@ -28,3 +28,33 @@ def test_qtde_de_spam(sessao, usuarios):
         'Confira os módulos fantasticos'
     )
     assert len(usuarios) == enviador.qtd_email_enviados
+
+
+class EnviadorMock(Enviador):
+
+    def __init__(self):
+        super().__init__()
+        self.qtd_email_enviados = 0
+        self.parametros_de_envio = None
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
+        self.qtd_email_enviados += 1
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Osvaldo', email='osvaldogpires@gmail.com')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'silvia1957@gmail.com',
+        'Curso Python Pro',
+        'Confira os módulos fantasticos'
+    )
+    assert enviador.parametros_de_envio == (
+        'silvia1957@gmail.com',
+        'osvaldogpires@gmail.com',
+        'Curso Python Pro',
+        'Confira os módulos fantasticos'
+    )
