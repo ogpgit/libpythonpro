0561547b5d6fa2bdf57f65d249e991297370aef4
ogpgit <osvaldogpires@gmail.com>
2020-11-08 11:29:33 -0300

    Implementando Módulo como Objeto - Nesse tópico você vai compreender a afirmação: "Tudo em Python é um objeto".

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -2,7 +2,7 @@ from unittest.mock import Mock
 
 import pytest
 
-from libpythonpro.spam.enviador_email import Enviador
+# from libpythonpro.spam.enviador_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
 from libpythonpro.spam.modelos import Usuario
 
---
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -0,0 +1,13 @@
+from unittest.mock import Mock
+
+from libpythonpro import github_api
+
+
+def test_buscar_avatar():
+    resp_mock = Mock()
+    resp_mock.json.return_value = {
+        'avatar_url': 'https://avatars1.githubusercontent.com/u/67315703?v=4'
+    }
+    github_api.requests.get = Mock(return_value=resp_mock)
+    url = github_api.buscar_avatar('ogpgit')
+    assert 'https://avatars1.githubusercontent.com/u/67315703?v=4' == url
