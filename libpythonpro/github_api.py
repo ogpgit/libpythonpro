6c1b55250ee3f0b4f7e01d35aab76e3c2145c97d
ogpgit <osvaldogpires@gmail.com>
2020-11-10 07:09:17 -0300

    Implementando Módulo como Objeto - Nesse tópico você vai compreender a afirmação: "Tudo em Python é um objeto".

---
+++ libpythonpro/github_api.py
@@ -0,0 +1,24 @@
+import requests
+
+def buscar_avatar(usuario):
+    """
+    Buscar o avatar de um usuário no Github
+
+    :param usuario: str com o nome do usuário no github
+    :return: str com o link do avatar
+    """
+
+    url = f'https://api.github.com/users/{usuario}'
+
+    resp = requests.get(url)
+    # print(resp)
+    # print(resp.cookies)
+    # print(resp.json())
+    # r = resp.json()
+    # print(r)
+    # print(r['avatar_url'])
+    return resp.json()['avatar_url']
+
+
+if __name__ == '__main__':
+    print(buscar_avatar('ogpgit'))
